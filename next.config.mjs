7a174cd3974c4684fd93cd7f7df1fcb1c89b632b
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-11-04 00:09:59 -0300

    build: image remore patterns

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: process.env.URL.split(':')[0],
+        hostname: process.env.URL.split('//')[1].split(':')[0],
+        port: process.env.URL.split(':')[2],
+      },
+    ],
+  }
 }
 
 export default withPayload(nextConfig)
