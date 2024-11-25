478e97bdff7feea482df0b336ec185d1ea80d866
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-11-24 23:59:21 -0300

    feat: footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Footer({socials}:any) {
+
+  return (
+    <div className='w-full bg-brand h-24'>
+        <div className='container h-full'>
+            <div className=' flex justify-center items-center gap-8 h-full'>
+                {socials.links.map((link: any) => (
+                    <Link href={link.link.url} target='_blank' className='hover:underline text-background'>
+                        {link.link.text}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
