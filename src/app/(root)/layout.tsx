61f17c22d94bcaf4bcb85ddafac5ea34e9fda308
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-11-24 23:59:36 -0300

    fix: layout fixes with footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import { fetchCategories } from "@/actions";
+import { fetchCategories, fetchSocials } from "@/actions";
 import CartProvider, { CartContext } from "@/context/cart";
 import { Toaster } from "@/components/ui/toaster";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,17 +22,23 @@ export const metadata: Metadata = {
 
 
     const categories = await fetchCategories()
+    const socials = await fetchSocials()
 
     return (
       <html lang="pt-br">
         <body
           className={`${inter.className} bg-slate-50 overflow-x-hidden`}
         >
+          <div className="flex flex-col min-h-screen">
           <CartProvider>
           <Navbar categories={categories} />
+          <div className="flex-1">
           {children}
+          </div>
           <Toaster />
           </CartProvider>
+          <Footer socials={socials}/>
+          </div>
         </body>
       </html>
     );
