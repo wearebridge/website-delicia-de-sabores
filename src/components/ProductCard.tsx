f58288cf4d71baa17750128d27f6ae37915a8f47
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-11-27 23:00:47 -0300

    test: #1

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,16 +5,20 @@ import ProductDetails from './ProductDetails'
 
 function ProductCard({product}: {product: any}) {
 
-  const imageUrl = product.image?.url
-    ? `${process.env.URL}${product.image.url}`
-    : '/assets/placeholder.png'
+  console.log(process.env.URL)
+  console.log(product.image?.url)
+
+
+  // const imageUrl = product.image?.url
+  //   ? `${process.env.URL}${product.image.url}`
+  //   : '/assets/placeholder.png'
 
   return (
     <Card>
         <CardContent className='flex flex-col aspect-square justify-center p-3 md:p-6'>
              <div className='min-h-48 h-full md:max-h-80 w-full relative mx-auto'>
                 <Image
-                    src={imageUrl}
+                    src={product.image?.url ? `${process.env.URL}${product.image.url}` : '/assets/placeholder.png'}
                     alt={product.name}
                     fill
                     className='object-center object-cover rounded-md'
